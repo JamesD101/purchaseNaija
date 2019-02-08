b4671774484287b24a04a79025d57bd647b7f7b6
JamesD101 <jamesodeyale01@gmail.com>
2019-02-09 00:49:23 +0100

    New commi 9-15-2018

--- api/v1/routes/customer.js
+++ api/v1/routes/customer.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const Products = require('../models/products');
+const Review = require('../models/review');
+const UserCart = require('../models/userCart');
 const config = require('../config/secret');
 const jwt = require('jsonwebtoken');
 
@@ -99,7 +101,8 @@ module.exports = function(router){
                             } else {
                                 const token = jwt.sign({ userId: user._id}, config.secretKey, {expiresIn: '5h'});
                                 res.json({ success: true, message: 'Success!', token: token, user: {
-                                        id: user._id
+                                        id: user._id,
+                                        email: user.email
                                     }});
                             }
                         }
@@ -199,7 +202,7 @@ module.exports = function(router){
         });
     });
 
-    //get all products
+    //Endpoint to get all products
     router.get('/products', function (req,res) {
        Products.find({}, function (err, products) {
            if (err) {
@@ -220,7 +223,7 @@ module.exports = function(router){
        })
     });
 
-    //Get a particular product by ID
+    //Endpoint to Get a particular product by ID
 
     router.get('/products/:id', function (req,res) {
         Products.find({ _id: req.params.id}, function (err, products) {
@@ -241,7 +244,7 @@ module.exports = function(router){
         })
     });
 
-    //Get a particular product by productName
+    //Endpoint to Get a particular product by productName
     router.get('/product/search/:name?', function (req,res) {
         var pname = req.query.name;
         Products.find({name: pname}, function (err, products) {
@@ -264,7 +267,172 @@ module.exports = function(router){
         })
     });
 
-    //API to get a particular user
+    //Endpoint to add User Cart
+    router.post('/userCart/:userEmail?/:productID?', function (req, res) {
+        var status = "PENDING";
+        var userCart = new UserCart({
+            userEmail: req.query.userEmail,
+            productID: req.query.productID,
+            quantity: req.body.quantity,
+            status: status
+        });
+        userCart.save(function (err) {
+            if (err) {
+                res.json({success:false, message: err});
+            } else {
+                res.json({success: true, message: 'Item has been added to cart'});
+            }
+        });
+    });
+
+    //Endpoint to  to get User Cart
+
+    // Using productID you can get the product itself by using the get product using ID end point
+
+    router.get('/userCart/:userEmail', function (req,res) {
+       UserCart.find({userEmail: req.params.userEmail}, function (err, userCart) {
+           if (err) {
+               res.json({success: false, message: err});
+           } else {
+               if (!userCart) {
+                   res.json({success: false, message: "User's cart does not exist"});
+               }
+               else {
+                   if (userCart.length == 0) {
+                       res.json({success: false, message: "No product in the user's cart"});
+                   }
+                   else {
+                       res.json({success: true, userCart: userCart});
+                   }
+
+               }
+           }
+       }) ;
+    });
+
+    //Endpoint to Update User cart
+
+    router.put('/userCart/:userEmail?/:productID?', function (req, res) {
+        UserCart.findOne({userEmail: req.query.userEmail, productID:req.query.productID}, function (err,cart) {
+            if(err) {
+                res.json({success: false, message: err});
+            }else {
+                var cartID = cart._id;
+                UserCart.findByIdAndUpdate(cartID, {
+                    userEmail: req.query.userEmail,
+                    productID: req.query.productID,
+                    quantity: req.body.quantity
+                    // status: status
+                }, function (err) {
+                    if (err) {
+                        res.json({success: false, message: 'Cart could not be updated'});
+                    } else {
+                        res.json({success: true, message: 'Cart has been Updated'});
+                    }
+                });
+            }
+        });
+    });
+
+    //Endpoint to Delete a userCart
+
+    router.delete('/userCart/:userEmail?/:productID?', function (req,res) {
+        UserCart.findOne({userEmail: req.query.userEmail, productID:req.query.productID}, function (err,cart) {
+            if(err) {
+                res.json({success: false, message: err});
+            }else {
+                var cartID = cart._id;
+                UserCart.findByIdAndRemove({_id: cartID}).then(function (done) {
+                    if (done) {
+                        res.json({success: true, message: 'Cart was successfully deleted'});
+                    } else {
+                        res.json({success: false, message: 'An error occurred. Try again later.'});
+                    }
+
+                });
+            }
+        });
+    });
+
+    //Create comment by user Endpoint
+
+    router.post('/review/:userEmail', function (req, res) {
+       if(!req.body.productID) {
+            res.json({success: false, message: 'Product ID is required'});
+       } else {
+           if (!req.body.review) {
+               res.json({success: false, message: 'Reivew is required'});
+           } else {
+               if (!req.body.numberOfStars) {
+                   res.json({success: false, message: 'Rating is required'});
+               } else {
+                   let review = new Review({
+                      userEmail: req.params.userEmail,
+                      productID: req.body.productID,
+                       review: req.body.review,
+                       numberOfStars: req.body.numberOfStars,
+                       createdAt: Date.now()
+                   });
+                   review.save(function (err) {
+                      if (err) {
+                          res.json({success: false, message: err});
+                      } else {
+                          res.json({success: true, message: 'Review has been saved'});
+                      }
+                   });
+               }
+           }
+       }
+    });
+
+
+    //Endpoint to get reviews for a product
+    router.get('/review/:productID', function(req, res) {
+        Review.find({productID: req.params.productID}, function (err, review) {
+            if (err){
+                res.json({success:false, message: err});
+            } else {
+                if (!review){
+                    res.json({success: false, message: 'No reivew was found for this user'});
+                } else {
+                    res.json({success: true, review: review});
+                }
+            }
+        })
+    });
+
+    //  Endpoint to get reviews for a single user
+    router.get('/getreview/:userEmail', function(req, res) {
+        Review.find({userEmail: req.params.userEmail}, function (err, review) {
+            if (err){
+                res.json({success:false, message: err});
+            } else {
+                if (!review){
+                    res.json({success: false, message: 'No reivew was found for this user'});
+                } else {
+                    res.json({success: true, review: review});
+                }
+            }
+        })
+    });
+
+    // Endpoint to get all reviews
+    router.get('/allreview', function(req, res) {
+        Review.find({}, function (err, review) {
+            if (err){
+                res.json({success:false, message: err});
+            } else {
+                if (!review){
+                    res.json({success: false, message: 'No reivew was found for this user'});
+                } else {
+                    res.json({success: true, review: review});
+                }
+            }
+        })
+    });
+
+
+    //Endpoint to get a particular user
 
     router.get('/user/:id', function (req, res) {
         User.findOne({ id: req.params.id}, function (err, user) {
